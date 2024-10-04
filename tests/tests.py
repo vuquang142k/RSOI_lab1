195f6ef7a4d8e59802ca2cbc28b0c5c7b25e00bb
Quang <kanji14092000@gmail.com>
2024-10-04 18:33:38 +0300

    Update

--- tests/tests.py
+++ tests/tests.py
@@ -24,7 +24,7 @@ patch_age = {"age": 21}
 
 @pytest.mark.parametrize("persons", [person1, person2])
 def test_post_get(persons):
-    r = requests.post(url="http://localhost:3000/api/v1/persons", json=persons)
+    r = requests.post(url="http://127.0.0.1:8080/api/v1/persons", json=persons)
     assert r.status_code == 201
     redirected_urd = r.headers['Location']
     person_id_dict = {"id": int(redirected_urd.split("/")[-1])}
@@ -36,7 +36,7 @@ def test_post_get(persons):
 @pytest.mark.parametrize("persons, patch_var", [(person1, patch_name), (person1, patch_address),
                                                 (person2, patch_work), (person2, patch_age)])
 def test_post_patch(persons, patch_var):
-    r = requests.post(url="http://localhost:3000/api/v1/persons", json=persons)
+    r = requests.post(url="http://127.0.0.1:8080/api/v1/persons", json=persons)
     redirected_urd = r.headers['Location']
     r = requests.patch(url=redirected_urd, json=patch_var)
     print(r.content)
@@ -47,10 +47,9 @@ def test_post_patch(persons, patch_var):
 
 @pytest.mark.parametrize("persons", [person1, person2])
 def test_post_delete(persons):
-    r = requests.post(url="http://localhost:3000/api/v1/persons", json=persons)
+    r = requests.post(url="http://127.0.0.1:8080/api/v1/persons", json=persons)
     redirected_urd = r.headers['Location']
     r = requests.delete(redirected_urd)
     assert r.status_code == 204
     r = requests.get(redirected_urd)
     assert r.status_code == 400
-
